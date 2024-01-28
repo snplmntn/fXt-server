895c4d73ab955b736422a8273c0c000ec9650e2a
snplmntn <124994202+snplmntn@users.noreply.github.com>
2024-01-29 03:00:08 +0800

    Add files via upload

--- routes/order/review.js
+++ routes/order/review.js
@@ -1,9 +1,6 @@
 const router = require("express").Router();
 const reviewController = require("../../controller/order/ReviewController");
 
-// Get Review/s
-router.get("/", reviewController.review_get);
-
 // Review
 router.post("/", reviewController.review_post);
 
